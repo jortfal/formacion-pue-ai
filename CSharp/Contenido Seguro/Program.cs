3a3e212795c0cb69db0509a6e55fdde428aae848
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-10-10 19:24:04 +0200

    Commit 10/10/2024

--- CSharp/Contenido Seguro/Program.cs
+++ CSharp/Contenido Seguro/Program.cs
@@ -1,18 +1,55 @@
 ﻿using Azure.AI.ContentSafety;
 using Azure;
+using Newtonsoft.Json;
+using System.Text;
 
-namespace Contenido_Seguro 
+namespace Contenido_Seguro
 {
     class Program
     {
+        private static string url = "<endpoint>";
+        private static string clave = "<key1 or key2>";
+
+        /// <summary>
+        /// Método Principal
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string url = "<end point>";
-            string clave = "<key1 or key2>";
-
-            var cliente = new ContentSafetyClient(new Uri(url), new AzureKeyCredential(clave));
-
             string texto = "Eres un poco idiota ...";
+            RunREST(texto);
+        }
+
+        /// <summary>
+        /// Utilización del servicio de Seguridad del Contenido mediante API Rest
+        /// </summary>
+        /// <param name="texto"></param>
+        public static void RunREST(string texto)
+        {
+            HttpClient http = new();
+            http.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", clave);
+
+            string endpoint = $"{url}/contentsafety/text:analyze?api-version=2023-10-01";
+            
+            var playload = new { text = texto };
+            string jsonPlayLoad = JsonConvert.SerializeObject(playload);
+            StringContent requestData = new StringContent(jsonPlayLoad, Encoding.UTF8, "application/json");
+
+            var response = http.PostAsync(endpoint, requestData).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            else Console.WriteLine($"Error {(int)response.StatusCode}: {response.StatusCode}");
+        }
+
+        /// <summary>
+        /// Utilización del servicio de Seguridad del Contenido mediante SDK
+        /// </summary>
+        /// <param name="texto"></param>
+        public static void RunSDK(string texto)
+        {
+            var cliente = new ContentSafetyClient(new Uri(url), new AzureKeyCredential(clave));    
             var consulta = new AnalyzeTextOptions(texto);
 
             try
--- Python/Contenido Seguro/Analizar-Texto.py
+++ Python/Contenido Seguro/Analizar-Texto.py
@@ -2,20 +2,45 @@ from azure.core.credentials import AzureKeyCredential
 from azure.core.exceptions import HttpResponseError
 from azure.ai.contentsafety import ContentSafetyClient
 from azure.ai.contentsafety.models import TextCategory, AnalyzeTextOptions
+import requests
+import json
 
-url = "<end point>"
+url = "<endpoint>"
 clave = "<key1 or key2>"
 
-cliente = ContentSafetyClient(url, AzureKeyCredential(clave))
+# Utilizar los servicios de Seguridad de contenido mediante SDK
+def RunSDK(texto):
+    cliente = ContentSafetyClient(url, AzureKeyCredential(clave))
+    consulta = AnalyzeTextOptions(text=texto)
 
+    try:
+        resultado = cliente.analyze_text(consulta)
+
+        for item in resultado.categories_analysis:
+            print(f"{item.category}: {item.severity}")
+
+    except HttpResponseError as e:
+        print(f"Error {e.error.code}: {e.error.message}")
+
+# Utilizar los servicios de Seguridad de contenido mediante API Rest
+def RunREST(texto):
+    headers = {
+        "Ocp-Apim-Subscription-Key": clave,
+        "Content-Type": "application/json"
+    }
+
+    playload = {
+        "text": texto
+    }
+
+    response = requests.post(f"{url}/contentsafety/text:analyze?api-version=2023-10-01", 
+                             headers=headers, data= json.dumps(playload))
+    
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error {response.status_code}: ")
+
+# Probar código
 texto = "Eres un poco idiota ..."
-consulta = AnalyzeTextOptions(text=texto)
-
-try:
-    resultado = cliente.analyze_text(consulta)
-
-    for item in resultado.categories_analysis:
-        print(f"{item.category}: {item.severity}")
-
-except HttpResponseError as e:
-    print(f"Error {e.error.code}: {e.error.message}")
+RunREST(texto)
